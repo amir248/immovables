5714648bf551cff2f177077dff4b27557c5fcd1c
chikchicly <chikchicly@gmail.com>
2025-03-07 11:52:34 +0700

    opacity

--- site/js/main.js
+++ site/js/main.js
@@ -5,22 +5,42 @@ function first(){
             firstScript.src='site/js/choiceLanguage.js';
             document.querySelector('body').append(firstScript);
         }
-        resolve(firstOk());
+        setTimeout(()=>{
+            resolve(firstOk());
+        },0);
     });
 }//first
-function second(){
+function languagaes(){
     return new Promise((resolve)=>{
         function secondFunction(){
             const secondScript=document.createElement('script');
             secondScript.src="menu/javascript-menu-for-landing.js";
             document.querySelector('body').append(secondScript);
         }
-        resolve(secondFunction());
-    })
-}
-
+        setTimeout(()=>{
+            resolve(secondFunction());
+        },1777);
+    });
+};//second
+function nav(){
+    return new Promise((resolve)=>{
+        function navFunction(){
+            const listNav=document.createElement('script');
+            listNav.src="site/js/includeHtmlToHtml.js";
+            document.querySelector('body').append(listNav);
+        }
+            resolve(navFunction());
+        setTimeout(()=>{
+                document.querySelector('header > nav').style.cssText=`
+                transition: all 1s ease-out;
+                opacity: 1;
+            `;
+        },1970);
+    });
+};//nav
 async function mainFunction(){
+    await nav();
+    await languagaes();
     await first();
-    await second();
 }
 mainFunction();
